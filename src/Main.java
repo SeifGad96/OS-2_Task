a9db80a19297ecd45b4cf6731cf1db1d6b196f8f
SeifGad96 <seifgad96@gmail.com>
2024-11-29 18:48:10 +0200

    setup main.yml

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Seif El-Deen Al-Sayed Mohamed");
-        
+        System.out.println("Enter 1st & 2nd name:");
+        String fullName = scanner.nextLine();
+
+        String[] names = fullName.split(" ");
+        String firstName = names[0];
+        String lastName = names[1];
+
+        System.out.println("first name: " + firstName);
+        System.out.println("second name: " + lastName);
+
     }
 }
